1a816c29e1ef0158e28cdbc3070132e294d1d450
Andrey Zlotnikov <andrey.zlotnikov@testpro.io>
2023-12-05 15:34:52 -0600

    Homework17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,2 +1,16 @@
-public class Homework17 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework17 extends BaseTest {
+
+    @Test
+    public void 
+
 }
