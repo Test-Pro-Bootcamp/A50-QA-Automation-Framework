09ddd23b2798eabceb61114e8e013e36db84a657
Svyeta0210 <son.nam.kim@testpro.io>
2023-12-05 22:37:35 -0500

    Edited Homewor17: deleted openWebrowser( ) method

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -7,7 +7,7 @@ public class Homework17 extends BaseTest{
 
 @Test(testName = "Add a song to the playlist", groups = "Smoke")
   public void addSongToPlaylist() throws InterruptedException {
-    openWebBrowser();
+
     enterEmail("son.nam.kim@testpro.io");
     enterPassword("koel08/23");
     clickLoginButton();
