db7fd67cee279a08c415c3803828b46ee0f2f7ae
Svyeta0210 <son.nam.kim@testpro.io>
2023-12-07 02:04:08 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
@@ -34,7 +32,7 @@ public class BaseTest {
     }
 
     public WebDriver getDriver() {
-        return getDriver();
+        return driver;
     }
 
     @AfterMethod
