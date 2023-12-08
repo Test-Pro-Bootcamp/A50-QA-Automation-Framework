c6d23e4825b81ef5d47536fc35a854de0d2cb61a
Andrey Zlotnikov <andrey.zlotnikov@testpro.io>
2023-12-07 23:08:57 -0600

    TestNG

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -6,7 +6,7 @@ public class Homework19 extends BaseTest {
     public void deletePlaylist () throws InterruptedException {
         logIn("andrey.zlotnikov@testpro.io", "Man07hat01tan26!");
         createPlaylist("Experimental");
-
+        deleteSelectedPlaylist("Experimental");
 
     }
 
