36877d8097c8401efcc5478cc29dd62791e80e6c
Svyeta0210 <son.nam.kim@testpro.io>
2023-12-22 22:43:15 -0500

    refactoring for page factory

--- src/main/java/com/qa/koel/AllSongsPage.java
+++ src/main/java/com/qa/koel/AllSongsPage.java
@@ -4,41 +4,57 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
 public class AllSongsPage extends BasePage {
-    static final By SIDE_PANEL_LIST_ALL_SONGS = By.cssSelector("a[class='songs']");
-    static final By FIRST_SONG_IN_ALL_SONGS = By.cssSelector
-            ("#songsWrapper > div > div > div > table > tr:nth-child(1) > td.title");
-    static final By GREEN_BUTTON_ADD_TO = By.cssSelector("button[class='btn-add-to']");
-    static final By DROPDOWN_MENU_PLAYLIST_NEW_P = By.cssSelector
-            ("#songsWrapper > header > div > div > section > ul > li.playlist");
-    static final  By OPTION_PLAY_IN_DROPDOWN_MENU = By.cssSelector("#app>nav>ul>li.playback");
-    static final By SOUND_BARS_IMAGE = By.cssSelector("div>img");
-    static final By ADDED_FIRST_SONG_TO_SIMPLE_CREATED_PLAYLIST = By.cssSelector
-            ("#playlistWrapper>div>div>div>table>tr>td.title");
+    @FindBy(css = "a[class='songs']")
+    WebElement sidePanelListAllSongs;
+    //static final By SIDE_PANEL_LIST_ALL_SONGS = By.cssSelector();
+    @FindBy(css = "#songsWrapper > div > div > div > table > tr:nth-child(1) > td.title")
+    WebElement firstSongInAllSongs;
+    //static final By FIRST_SONG_IN_ALL_SONGS = By.cssSelector();
+    @FindBy(css = "button[class='btn-add-to']")
+    WebElement greenButtonAddTo;
+    //static final By GREEN_BUTTON_ADD_TO = By.cssSelector();
+    @FindBy(css = "#songsWrapper > header > div > div > section > ul > li.playlist")
+    WebElement dropdownMenuPlaylistNewP;
+    //static final By DROPDOWN_MENU_PLAYLIST_NEW_P = By.cssSelector();
+    @FindBy(css = "#app>nav>ul>li.playback")
+    WebElement optionPlayInDropdownMenu;
+    //static final  By OPTION_PLAY_IN_DROPDOWN_MENU = By.cssSelector();
+    @FindBy(css = "div>img")
+    WebElement soundBarsImage;
+    //static final By SOUND_BARS_IMAGE = By.cssSelector();
+    @FindBy(css = "#playlistWrapper>div>div>div>table>tr>td.title")
+    WebElement addedFirstSongToSimpleCreatedPlaylist;
+    //static final By ADDED_FIRST_SONG_TO_SIMPLE_CREATED_PLAYLIST = By.cssSelector();
 
     public AllSongsPage(WebDriver driver) {
         super(driver);
     }
-    public void clickSidePanelListAllSongs(){
-        WebElement sidePanelListAllSongs = pageDriver.findElement(SIDE_PANEL_LIST_ALL_SONGS);
-        sidePanelListAllSongs.click();
+    public WebElement clickSidePanelListAllSongs(){
+        return sidePanelListAllSongs;
+//        WebElement sidePanelListAllSongs = pageDriver.findElement(SIDE_PANEL_LIST_ALL_SONGS);
+//        sidePanelListAllSongs.click();
     }
-    public void clickFirstSongInAllSongs(){
-        WebElement firstSongInAllSongsPlaylist = pageDriver.findElement(FIRST_SONG_IN_ALL_SONGS);
-        firstSongInAllSongsPlaylist.click();
+    public WebElement clickFirstSongInAllSongs(){
+        return firstSongInAllSongs;
+//        WebElement firstSongInAllSongsPlaylist = pageDriver.findElement(FIRST_SONG_IN_ALL_SONGS);
+//        firstSongInAllSongsPlaylist.click();
     }
-    public void clickGreenButtonAddTo(){
-        WebElement greenButtonAddTo = pageDriver.findElement(GREEN_BUTTON_ADD_TO);
-        greenButtonAddTo.click();
+    public WebElement clickGreenButtonAddTo(){
+        return clickGreenButtonAddTo();
+//        WebElement greenButtonAddTo = pageDriver.findElement(GREEN_BUTTON_ADD_TO);
+//        greenButtonAddTo.click();
     }
-    public void clickDropdownMenuPlaylistNewP(){
-        WebElement playlistNewPinDropDownMenu = pageDriver.findElement(DROPDOWN_MENU_PLAYLIST_NEW_P);
-        playlistNewPinDropDownMenu.click();
+    public WebElement clickDropdownMenuPlaylistNewP(){
+        return clickDropdownMenuPlaylistNewP();
+//        WebElement playlistNewPinDropDownMenu = pageDriver.findElement(DROPDOWN_MENU_PLAYLIST_NEW_P);
+//        playlistNewPinDropDownMenu.click();
     }
-    public boolean getAddedFirstSongToSimpleCreatedPlaylist(){
-        WebElement addedFirstSongFromAllSongs = pageDriver.findElement(ADDED_FIRST_SONG_TO_SIMPLE_CREATED_PLAYLIST);
-        return addedFirstSongFromAllSongs.isDisplayed();
+    public WebElement getAddedFirstSongToSimpleCreatedPlaylist(){
+//        WebElement addedFirstSongFromAllSongs = pageDriver.findElement(ADDED_FIRST_SONG_TO_SIMPLE_CREATED_PLAYLIST);
+        return addedFirstSongFromAllSongs();
     }
     public void contextClickFirstSongInAllSongsPlaylist(){
         WebElement firstSongInAllSongsPlaylist = pageDriver.findElement(By.cssSelector
--- src/main/java/com/qa/koel/BasePage.java
+++ src/main/java/com/qa/koel/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,6 +18,7 @@ public class BasePage {
         this.pageDriver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         actions = new Actions(driver);
+        PageFactory.initElements(pageDriver,this);
     }
     public void click(By locator){
         wait.until(ExpectedConditions.elementToBeClickable(locator));
